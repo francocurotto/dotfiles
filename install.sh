fe9386643a620e786808817f5dc3d812ca55bb60
Franco Curotto <francocurotto@gmail.com>
2020-03-22 23:25:58 +0900

    first commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+#/!bin/bash
+
+# install tmux config file
+ln -s $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
+
+# install inputrc config file
+ln -s $HOME/.dotfiles/inputrc $HOME/.inputrc
+
+# install bash configuration file. Intead of doing a ln -s
+# we opt to add source into a existing .bashrc given by the
+# distribution
+BASHLINE="source $HOME/.dotfiles/bashrc"
+# add line only if it doesn't already exists in .bashrc
+if ! grep -Fxq "$BASHLINE" $HOME/.bashrc; then
+    echo $'\n# Add my bash config file' >> $HOME/.bashrc
+    echo $BASHLINE >> $HOME/.bashrc
+fi
